9ac4a8da4c766e77fbb6b2fc1381667eb0bf4da2
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2020-07-27 18:26:19 -0500

    Create vertexTexture.js

---
+++ shaders/vertexTexture.js
@@ -0,0 +1,12 @@
+const vsTexture=`
+attribute vec4 aVertexPosition;
+attribute vec2 aTextureCoord;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+varying lowp vec2 vTextureCoord;
+void main(){
+    gl_Position=uProjectionMatrix
+               *uModelViewMatrix
+               *aVertexPosition;
+    vTextureCoord=aTextureCoord;
+}`
