b63d783f828fe9b4cd2c308af69677c2b0e5be6d
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2021-11-20 19:46:54 -0600

    Update vertexColor.js

--- shaders/vertexColor.js
+++ shaders/vertexColor.js
@@ -3,10 +3,13 @@ attribute vec4 aVertexPosition;
 attribute vec4 aVertexColor;
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
-varying lowp vec4 vColor;
+uniform float uPoinSize;
+
+varying mediump vec4 vColor;
 void main(){
     gl_Position=uProjectionMatrix
                *uModelViewMatrix
                *aVertexPosition;
     vColor=aVertexColor;
+    gl_PointSize = uPointSize;
 }`
