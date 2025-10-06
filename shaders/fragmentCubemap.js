95758fe4d094bfab8f8232a9a44d90a5c7aa3c6c
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-10-05 19:16:58 -0500

    Update and rename fsCubemap.js to fragmentCubemap.js

--- shaders/fragmentCubemap.js
+++ shaders/fragmentCubemap.js
@@ -1,10 +1,9 @@
-`
+const fsCubeMap = `
 precision highp float;
-
 uniform samplerCube uCubeMap;
 varying vec3 vDirection;
 
 void main() {
     gl_FragColor = textureCube(uCubeMap, normalize(vDirection));
-}
-`
+}`;
+
