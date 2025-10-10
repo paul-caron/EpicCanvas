c1a86ea5f00610fa7e4b81be93e5b873a7e8cafc
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-10-10 13:31:00 -0500

    Create fragmentPhongTexture.js

---
+++ shaders/fragmentPhongTexture.js
@@ -0,0 +1,44 @@
+const fsPhongTexture= `
+precision mediump float;
+
+varying vec3 vNormal;
+varying vec3 vWorldPosition;
+varying vec3 vViewDirection;
+varying vec2 vTextureCoord;
+
+uniform sampler2D uSampler;
+uniform vec3 pointLightPosition;
+uniform vec3 pointLightColor;
+uniform vec3 directionalLightColor;
+uniform vec3 directionalVector;
+uniform vec3 ambientLight;
+//uniform float shininess;
+
+void main() {
+	float shininess = 112.0;
+    vec3 normal = normalize(vNormal);
+    vec3 lightDir = normalize(pointLightPosition - vWorldPosition);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    vec3 viewDir = normalize(vViewDirection);
+    vec4 textureColor = texture2D(uSampler, vTextureCoord);
+
+    // Ambient
+    vec3 ambient = ambientLight * textureColor.rgb;
+    
+    // Directional
+    float directionalLightIntensity=max(dot(normal, directionalVector),0.0);
+    vec3 directional = directionalLightIntensity * directionalLightColor * textureColor.rgb;
+
+    // Diffuse
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = pointLightColor * diff * textureColor.rgb;
+
+    // Specular
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    vec3 specular = pointLightColor * spec * textureColor.rgb;
+
+    // Combine
+    vec3 finalColor = ambient + diffuse + specular + directional;
+    gl_FragColor = vec4(finalColor, textureColor.a);
+}
+`;
