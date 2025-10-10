c4237390dc09f5fd457d099f9bb0d51b15c132b4
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-10-10 13:29:59 -0500

    Create vertexPhongTexture.js

---
+++ shaders/vertexPhongTexture.js
@@ -0,0 +1,26 @@
+const vsPhongTexture = `
+attribute vec4 aVertexPosition;
+attribute vec4 aVertexNormal;
+attribute vec2 aTextureCoord;
+
+uniform mat4 uModelMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uProjectionMatrix;
+uniform mat4 uNormalMatrix;
+uniform vec3 uCameraPosition;
+
+varying vec3 vNormal;
+varying vec3 vWorldPosition;
+varying vec2 vTextureCoord;
+varying vec3 vViewDirection;
+
+void main() {
+    vec4 worldPosition = uModelMatrix * aVertexPosition;
+    vWorldPosition = worldPosition.xyz;
+    vNormal = normalize(uNormalMatrix * aVertexNormal).xyz;
+    vViewDirection = normalize(uCameraPosition - worldPosition.xyz);
+    vTextureCoord = aTextureCoord;
+    gl_Position = uProjectionMatrix * uViewMatrix * worldPosition;
+}
+
+`;
