943aedd610f3866e30d14f33ed8707de3e5dacd6
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-10-10 13:31:30 -0500

    Delete shaders/fragmentTextureLight.js

--- shaders/fragmentTextureLight.js
+++
@@ -1,8 +0,0 @@
-const fsTextureLight=`
-varying highp vec2 vTextureCoord;
-varying highp vec3 vLighting;
-uniform sampler2D uSampler;
-void main(void){
-    mediump vec4 texelColor=texture2D(uSampler,vec2(vTextureCoord.s,vTextureCoord.t));
-    gl_FragColor=vec4(texelColor.rgb*vLighting,texelColor.a);
-}`
