bacfaf5aca026066cc7e993c6e5033a5eed23cca
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-10-10 17:05:04 -0500

    Update vertexSkybox.js

--- shaders/vertexSkybox.js
+++ shaders/vertexSkybox.js
@@ -1,22 +1,19 @@
 const vsSkybox = `
 attribute vec4 aVertexPosition;
-
-uniform mat4 uModelMatrix;
 uniform mat4 uViewMatrix;
 uniform mat4 uProjectionMatrix;
 
 varying vec3 vDirection;
 
 void main() {
-    // Only apply rotation part of modelViewMatrix (remove translation)
-    mat4 viewRotation = mat4(mat3(uViewMatrix * uModelMatrix));
+    // Only apply rotation part of ViewMatrix (remove translation)
+    mat4 viewRotation = mat4(mat3(uViewMatrix));
 
     vec4 direction = viewRotation * aVertexPosition;
     vDirection = direction.xyz;
 
     gl_Position = uProjectionMatrix * viewRotation * vec4(direction.xyz, 1.0);
 }
-
 
 
 `;
