e002ae42abe226e70809e5bed227bf7730a11aea
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-10-15 09:54:21 -0500

    Update fragmentPhongTexture.js

--- shaders/fragmentPhongTexture.js
+++ shaders/fragmentPhongTexture.js
@@ -7,34 +7,34 @@ varying vec3 vViewDirection;
 varying vec2 vTextureCoord;
 
 uniform sampler2D uSampler;
-uniform vec3 pointLightPosition;
-uniform vec3 pointLightColor;
-uniform vec3 directionalLightColor;
-uniform vec3 directionalVector;
-uniform vec3 ambientLight;
-uniform float shininess;
+uniform vec3 uPointLightPosition;
+uniform vec3 uPointLightColor;
+uniform vec3 uDirectionalLightColor;
+uniform vec3 uDirectionalVector;
+uniform vec3 uAmbientLight;
+uniform float uShininess;
 
 void main() {
     vec3 normal = normalize(vNormal);
-    vec3 lightDir = normalize(pointLightPosition - vWorldPosition);
+    vec3 lightDir = normalize(uPointLightPosition - vWorldPosition);
     vec3 reflectDir = reflect(-lightDir, normal);
     vec3 viewDir = normalize(vViewDirection);
     vec4 textureColor = texture2D(uSampler, vTextureCoord);
 
     // Ambient
-    vec3 ambient = ambientLight * textureColor.rgb;
+    vec3 ambient = uAmbientLight * textureColor.rgb;
     
     // Directional
-    float directionalLightIntensity=max(dot(normal, directionalVector),0.0);
-    vec3 directional = directionalLightIntensity * directionalLightColor * textureColor.rgb;
+    float directionalLightIntensity=max(dot(normal, uDirectionalVector),0.0);
+    vec3 directional = directionalLightIntensity * uDirectionalLightColor * textureColor.rgb;
 
     // Diffuse
     float diff = max(dot(normal, lightDir), 0.0);
-    vec3 diffuse = pointLightColor * diff * textureColor.rgb;
+    vec3 diffuse = uPointLightColor * diff * textureColor.rgb;
 
     // Specular
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
-    vec3 specular = pointLightColor * spec * textureColor.rgb;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), uShininess);
+    vec3 specular = uPointLightColor * spec * textureColor.rgb;
 
     // Combine
     vec3 finalColor = ambient + diffuse + specular + directional;
